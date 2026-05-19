cbb7d3a2f6f26915881d6ca27a8328ab73077140
Ragavi14 <ragavi2002l@gmail.com>
2026-05-19 11:41:53 +0530

    error changes

--- server/server.js
+++ server/server.js
@@ -23,7 +23,15 @@ db.getConnection()
    MIDDLEWARE
 ========================= */
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type"]
+}));
+
+app.options("*", cors());
+
 app.use(express.json());
 
 /* =========================
