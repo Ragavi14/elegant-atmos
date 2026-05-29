8c7c7a594fdc0a27ad61444cd75a263aeca0a744
Ragavi14 <ragavi2002l@gmail.com>
2026-05-29 12:04:18 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -19,6 +19,50 @@ export default function App() {
     }
   }, []);
 
+ /* =========================
+     LAZY LOAD FACEBOOK PIXEL
+     (Fixes Mobile Performance)
+  ========================= */
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Setup base window tracking commands safely using rest parameters
+      if (!window.fbq) {
+        window.fbq = function (...args) {
+          if (window.fbq.callMethod) {
+            window.fbq.callMethod(...args);
+          } else {
+            window.fbq.queue.push(args);
+          }
+        };
+        window._fbq = window.fbq;
+        window.fbq.push = window.fbq;
+        window.fbq.loaded = true;
+        window.fbq.version = '2.0';
+        window.fbq.queue = [];
+      }
+
+      // Inject the external script tag safely
+      const scriptElement = document.createElement('script');
+      scriptElement.async = true;
+      scriptElement.src = 'https://connect.facebook.net/en_US/fbevents.js';
+      
+      const firstScript = document.getElementsByTagName('script')[0];
+      if (firstScript && firstScript.parentNode) {
+        firstScript.parentNode.insertBefore(scriptElement, firstScript);
+      } else {
+        document.head.appendChild(scriptElement);
+      }
+
+      // Initialize with your live Pixel ID
+      window.fbq('init', '1673726469900736'); 
+      window.fbq('track', 'PageView');
+
+      console.log("Facebook Pixel loaded cleanly after 3s delay.");
+    }, 3000); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
   /* =========================
      LOGIN HANDLER
   ========================= */
